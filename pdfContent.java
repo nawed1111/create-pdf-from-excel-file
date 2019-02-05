fa3c3684750b4fefd21dd16e00976c2269e398b9
nawed1111 <32324785+nawed1111@users.noreply.github.com>
2019-02-06 03:11:05 +0530

    Add files via upload

---
+++ pdfContent.java
@@ -0,0 +1,35 @@
+import com.itextpdf.text.pdf.PdfReader;
+import com.itextpdf.text.pdf.parser.PdfTextExtractor;
+
+import java.io.IOException;
+
+public class pdfContent {
+
+    private static final String FILE_NAME = "C:\\Users\\Nawed Anjum\\Pictures\\Monawwer\\Employee Photo ID and Access Card Requisition Form_locked_.pdf";
+
+    public static void main(String[] args) {
+
+        PdfReader reader;
+
+        try {
+
+            reader = new PdfReader(FILE_NAME);
+
+            // pageNumber = 1
+            String textFromPage = PdfTextExtractor.getTextFromPage(reader, 1);
+            System.out.println(textFromPage);
+            String arr[] = new String[10];
+            arr = textFromPage.split("\\n");
+            for(int i=0;i<arr.length;i++){
+            if(arr[i].contains("Career Level"))
+            System.out.println(arr[i]);
+            }
+            reader.close();
+
+        } catch (IOException e) {
+            e.printStackTrace();
+        }
+
+    }
+
+}
